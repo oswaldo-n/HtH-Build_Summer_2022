f85fce4d0f214baca9cdd87652226f3c2ea849ce
Oswaldo Navarrete <oswaldonavarrete139@gmail.com>
2022-08-10 16:29:45 -0700

    adding 'getting started with python' activity

---
+++ Getting_Started_With_Python/about_me.py
@@ -0,0 +1,20 @@
+full_name = "Oswaldo N. Navarrete"
+nickname = "Ozzy"
+age = 18
+has_used_python = True
+
+print("Hello World, my name is", full_name, "!")
+hobbies = [
+    "Photography", "Working Out"
+    "Producing Beats", "Coding", "Learning"
+]
+print(hobbies)
+
+fav_things = {
+    "Food": "Quesa Berrias",
+    "Movie": "October Sky",
+    "Book": "50 Shades of gray",
+    "Game": "Call of duty",
+}
+
+all_vars = dict(vars())
